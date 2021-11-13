3d0042dafc4ac5c68e6fc79a58e5aa951917e157
Viktoria <vikitoga@gmail.com>
2021-11-13 23:37:05 +0300

    Tests added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    private final CashbackHackService сashback = new CashbackHackService();
+
+    @Test
+    public void shouldTestRemain() {
+        int actual = сashback.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestLimit() {
+        int actual = сashback.remain(1000);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestOverLimit() {
+        int actual = сashback.remain(1001);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestUnderLimit() {
+        int actual = сashback.remain(999);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldTestZeroAmount() {
+        int actual = сashback.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+ }
